a9b3f4529bbb86ec949237e964b31e35c32bdcda
szymonszymonszymonv <kalarepka8799@gmail.com>
2021-04-20 18:45:17 +0200

    add bootstrap and create navbar for landing page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,10 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+// import '../import_bootstrap.js'  
+import 'jquery'
+import 'popper.js'
+import 'stylesheets/application'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
