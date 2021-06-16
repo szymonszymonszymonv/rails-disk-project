aecfcf8ee16e7e7d305a8263f7ddf7585640429f
szymonszymonszymonv <kalarepka8799@gmail.com>
2021-06-16 19:12:49 +0200

    modify migration

--- db/migrate/20210407152426_remove_modified_at_from_directory.rb
+++ db/migrate/20210407152426_remove_modified_at_from_directory.rb
@@ -1,5 +1,5 @@
 class RemoveModifiedAtFromDirectory < ActiveRecord::Migration[6.1]
   def change
-    remove_column :directories, :modified_at, :datetime
+    # remove_column :directories, :modified_at, :datetime
   end
 end
